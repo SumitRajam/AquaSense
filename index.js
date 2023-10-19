4191dbd52810a8d34484d1f6aebb814e26086c39
SumitRajam <113175272+SumitRajam@users.noreply.github.com>
2023-10-20 00:50:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+url = "http://10.53.6.179:80"
+
 function scrollToSection() {
     const scrollTarget = document.getElementById('scrollTarget');
     scrollTarget.scrollIntoView({ behavior: 'smooth' });
@@ -11,3 +13,15 @@ function startMarquee() {
 function Raise() {
         location.href = "Complaint.html";
 }
+
+// async function notifications() {
+//     const response = await fetch(`${url}/notifications`)
+//     const notifications = await response.json()
+//     return notifications.response
+// }
+
+// console.log(notifications())
+
+fetch(`${url}/notifications`)
+.then((response) => response.json())
+.then((data) => console.log(data.response))
